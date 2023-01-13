0217edc80b5cfdf34380bea815e34066c8905514
txcurri <thomascurrier@mail.weber.edu>
2023-01-12 19:07:26 -0700

    this is a test file

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,5 +1,6 @@
 print ('Hello I am Mr.C')
-print("""it is a big world
+print("""It is a big world
 and I like it""" )
 
-print('I am trying this out for the first time.'
+print('I am trying this out for the first time.')
+
