47c3c72e7a98d5badeb0e99209cfd2fa29ae5bdd
txcurri <thomascurrier@mail.weber.edu>
2023-01-13 18:20:31 -0700

    this is a test file

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,6 +1,7 @@
 print ('Hello I am Mr.C')
 print("""It is a big world
 and I like it""" )
-
+print('\nnew line')
+print("This\nis\nmy\nsimple\ncode.")
 print('I am trying this out for the first time.')
 
