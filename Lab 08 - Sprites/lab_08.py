585d26c0d8b1c4a834f0f3546ab87727f94ae08e
txcurri <thomascurrier@mail.weber.edu>
2023-03-24 16:39:13 -0600

    lab_08.py

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -1,14 +1,3 @@
-"""
-Sprite Collect Coins
-
-Simple program to show basic sprite usage.
-
-Artwork from https://kenney.nl
-
-If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.sprite_collect_coins
-"""
-
 import random
 import arcade
 
